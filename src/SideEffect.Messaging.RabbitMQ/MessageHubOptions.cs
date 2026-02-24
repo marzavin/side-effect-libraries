fb8f843f9752bfa75e80d4c30237d80baf741683
Artsiom Marzavin <marzavin@gmail.com>
2026-02-24 18:45:48 +0100

    Nuget deploy settings

--- src/SideEffect.Messaging.RabbitMQ/MessageHubOptions.cs
+++ src/SideEffect.Messaging.RabbitMQ/MessageHubOptions.cs
@@ -1,9 +1,18 @@
 namespace SideEffect.Messaging;
 
+/// <summary>
+/// Represents options for message hub.
+/// </summary>
 public class MessageHubOptions
 {
+    /// <summary>
+    /// Gets handler registry.
+    /// </summary>
     public IHandlerRegistry Registry { get; }
 
+    /// <summary>
+    /// Creates new instance of <see cref="MessageHubOptions"/>.
+    /// </summary>
     public MessageHubOptions()
     {
         Registry = new HandlerRegistry();
